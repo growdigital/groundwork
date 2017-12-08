4da4baf8ecef744b6501eeeb84aff338fc3da093
Jake Rayson <contact@growdigital.org>
2017-12-08 13:03:41 +0000

    fix(templates): add const values index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-var Metalsmith = require('metalsmith');
-var markdown = require('metalsmith-markdown');
-var layouts = require('metalsmith-layouts');
-var permalinks = require('metalsmith-permalinks');
+const Metalsmith = require('metalsmith');
+const cleancss = require('metalsmith-clean-css');
+const collection = require('metalsmith-collections');
+const concat = require('metalsmith-concat');
+const dateFormat = require('metalsmith-date-formatter');
+const drafts = require('metalsmith-drafts');
+const feed = require('metalsmith-feed');
+const markdown = require('metalsmith-markdown');
+const metadata = require('metalsmith-writemetadata');
+const move = require('metalsmith-movey').default;
+const permalinks = require('metalsmith-permalinks');
+const postcss = require('metalsmith-postcss');
+const templates = require('metalsmith-layouts');
+const uglifyjs = require('metalsmith-uglifyjs');
 
 Metalsmith(__dirname)
   .metadata({
@@ -16,7 +26,7 @@ Metalsmith(__dirname)
   .use(markdown())
   .use(permalinks())
   .use(
-    layouts({
+    templates({
       engine: 'handlebars'
     })
   )
